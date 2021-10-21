5ce58636e70b43ec34af363416605ee9769526b4
Jeroen Claassens <support@favware.tech>
2021-10-22 01:00:37 +0200

    fix: only download jetbrains mono from google fonts

--- src/components/HeadTags.tsx
+++ src/components/HeadTags.tsx
@@ -17,10 +17,7 @@ const HeadTags: FC = () => {
 			<link rel="canonical" href={BaseUrl} />
 			<link rel="preconnect" href="https://fonts.googleapis.com" />
 			<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-			<link
-				rel="stylesheet"
-				href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;700&family=JetBrains+Mono:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
-			/>
+			<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@300;400;500;700&display=swap" />
 			<link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
 			<link rel="icon" type="image/png" sizes="192x192" href="/icons/android-chrome-192x192.png" />
 			<link rel="icon" type="image/png" sizes="194x194" href="/icons/android-chrome-194x194.png" />
