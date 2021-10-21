85663d52e3957e6d151eb377a7151af30e1010b1
Jeroen Claassens <support@favware.tech>
2021-10-21 21:58:36 +0200

    chore: add logical and + logical or to script

--- scripts/make-djs-node-14-compatible.mjs
+++ scripts/make-djs-node-14-compatible.mjs
@@ -1,13 +1,28 @@
-import replace from 'replace-in-file';
-import { fileURLToPath } from 'node:url';
 import { sep } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import replace from 'replace-in-file';
 
 const discordJsSrcFolder = new URL('../node_modules/discord.js/src/', import.meta.url);
+const files = `${fileURLToPath(discordJsSrcFolder)}**${sep}*.js`;
 
-const options = {
-	files: `${fileURLToPath(discordJsSrcFolder)}**${sep}*.js`,
+const optionsLogicalNullish = {
+	files,
 	from: /([a-zA-Z].+) \?\?= (.+);/g,
-	to: 'if ($1 === null || $1 === undefined) $1 = $2;'
+	to: '$1 !== null && $1 !== void 0 ? $1 : ($1 = $2);'
 };
 
-await replace.replaceInFile(options);
+const optionsLogicalAnd = {
+	files,
+	from: /([a-zA-Z].+) \&\&= (.+);/g,
+	to: '$1 && ($1 = $2);'
+};
+
+const optionsLogicalOr = {
+	files,
+	from: /([a-zA-Z].+) \|\|= (.+);/g,
+	to: '$1 || ($1 = $2);'
+};
+
+await replace.replaceInFile(optionsLogicalNullish);
+await replace.replaceInFile(optionsLogicalAnd);
+await replace.replaceInFile(optionsLogicalOr);
